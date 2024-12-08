25a55f644c080f6f8e1305ee401cbec66d02142e
rlm <202312541@stu.neuq.edu.cn>
2024-12-09 01:22:40 +0800

    商品浏览业务功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -54,4 +54,11 @@ public interface DishService {
      * @param id
      */
     List<Dish> getBycategoryId(Long id);
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
