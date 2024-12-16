59a901a4e70659ee6f5b6b4dfda859d0510f9831
rlm <202312541@stu.neuq.edu.cn>
2024-12-16 23:54:24 +0800

    用户数据统计业务代码开发

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,6 +1,7 @@
 package com.sky.service;
 
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
 
@@ -13,4 +14,12 @@ public interface ReportService {
      * @return
      */
     TurnoverReportVO getTurnoverStatistics(LocalDate startDate, LocalDate endDate);
+
+    /**
+     * 获取指定时间段内用户统计数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 }
